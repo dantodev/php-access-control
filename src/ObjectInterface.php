19c3e43981932016901cefc4abfc62c2e8c2ea79
Daniel Kahl <kontakt@daniel-kahl.de>
2016-04-15 22:08:33 +0200

    #noticket * add Judge::addUser() * rename UserInterface to UserAccessInterface

--- src/ObjectInterface.php
+++ src/ObjectInterface.php
@@ -7,10 +7,10 @@ interface ObjectInterface
    * Returns an array of roles related through this object instance to given user instance.
    * Keep in mind that there are no recursive checks by AccessControl. You have to do this on your own in this method.
    *
-   * @param UserInterface $user
+   * @param UserAccessInterface $user
    * @return string[]
    */
-  public function getObjectRoles(UserInterface $user);
+  public function getObjectRoles(UserAccessInterface $user);
 
   /**
    * Returns an array of relevant objects related to this object instance.
