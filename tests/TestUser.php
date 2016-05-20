618db447820582b8599dd3c8c7da567e9609c371
daniel <kontakt@daniel-kahl.de>
2016-05-20 16:24:25 +0200

    * add PSR-2 compatibility #1

--- tests/TestUser.php
+++ tests/TestUser.php
@@ -6,16 +6,16 @@ use Dtkahl\AccessControl\UserAccessInterface;
 class TestUser implements UserAccessInterface
 {
 
-  private $global_roles = [];
+    private $global_roles = [];
 
-  public function __construct(array $global_roles)
-  {
-    $this->global_roles = $global_roles;
-  }
+    public function __construct(array $global_roles)
+    {
+        $this->global_roles = $global_roles;
+    }
 
-  public function getGlobalRoles()
-  {
-    return $this->global_roles;
-  }
+    public function getGlobalRoles()
+    {
+        return $this->global_roles;
+    }
 
 }
